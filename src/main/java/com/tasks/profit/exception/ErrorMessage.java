546965875596daf1c626c53c369348f203788fb7
ayntsbern <ayntsbern@users.noreply.github.com>
2019-08-13 23:04:32 +0300

    Do some corrects in ErrorMessage.

--- src/main/java/com/tasks/profit/exception/ErrorMessage.java
+++ src/main/java/com/tasks/profit/exception/ErrorMessage.java
@@ -3,14 +3,36 @@ package com.tasks.profit.exception;
 import javax.ws.rs.core.Response;
 
 public class ErrorMessage {
-    public String code;
-    public String description;
-    public String link;
-    public Response.Status status;
-    public ErrorMessage(String code, String description, String link, Response.Status status){
+    private String code;
+    private String description;
+    private Response.Status status;
+
+    public ErrorMessage(){}
+    public ErrorMessage(String code, String description, Response.Status status){
         this.code = code;
         this.description = description;
-        this.link = link;
         this.status = status;
+    }
+
+    public ErrorMessage get404(){
+        code = "404";
+        description = "Product not found!";
+        status = Response.Status.NOT_FOUND;
+        return this;
+    }
+
+    public ErrorMessage get409(String name){
+        code = "409";
+        description = "Product " + name + " already exists!";
+        status = Response.Status.CONFLICT;
+        return this;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Code: %s\n" +
+                "Description: %s\n" +
+                "Error status: %s",
+                code, description, status.toString());
     }
 }
